d27ac6ea4e4b4829b02fdc26db55a69b34f4aa4f
Slthemf <36306396+Slthemf@users.noreply.github.com>
2019-06-18 21:12:04 -0400

    delete menu function

--- finalproject.py
+++ finalproject.py
@@ -5,13 +5,10 @@ height = 500
 
 menu_page = 0
 game_page = 1
-store_page = 2
-game_over = 3
+game_over = 2
 
 
 class MyGame(arcade.Window):
-    coin_count = 0
-    highest_score = 0
 
     def __init__(self, width, height):
         super().__init__(width, height)
@@ -26,14 +23,6 @@ class MyGame(arcade.Window):
     def draw_menu(self):
         arcade.draw_rectangle_filled(self.width/2, self.height/2, 300, 60, arcade.color.DARK_GRAY)
         arcade.draw_text("Start game", self.width/2 - 110, self.height/2 - 20, arcade.color.WHITE, 36)
-        arcade.draw_rectangle_filled(self.width/2, self.height/3, 300, 60, arcade.color.DARK_BLUE_GRAY)
-        arcade.draw_text("Store", self.width / 2 - 25, self.height/3, arcade.color.WHITE, 18)
-        arcade.draw_text("Highest Score: ", self.width*4/5, self.height*2/3, arcade.color.BLACK)
-
-    def draw_store(self):
-        arcade.draw_text("Coin: ", self.width*3/4, self.height*5/6, arcade.color.WHITE, 18)
-        arrow_image = arcade.load_texture("Return_Arrow-512.png")
-        arcade.draw_texture_rectangle(100, 400, 40, 40, arrow_image)
 
     def draw_game_over(self):
         arcade.draw_text("Game Over", self.width/2, self.width/2, arcade.color.BRICK_RED, 54)
@@ -46,9 +35,6 @@ class MyGame(arcade.Window):
             self.draw_menu()
 
         elif self.page == 2:
-            self.draw_store()
-
-        elif self.page == 3:
             self.draw_game_over()
 
     def on_mouse_motion(self, position_x, position_y, dx, dy):
@@ -61,11 +47,7 @@ class MyGame(arcade.Window):
             if self.width/2 - 30 < self.position_x < self.width/2 + 30:
                 if self.height/2 - 10 < self.position_y < self.height/2 + 10:
                     self.page = 1
-                elif self.height/3 - 10 < self.position_y < self.height/3 + 10:
-                    self.page = 2
-        if self.page == 2:
-            if 80 < self.position_x < 120 and 380 < self.position_y < 420:
-                self.page = 0
+
         if self.page == 3:
             self.page = 0
 
